d0c4f2a5af8094435fd0e62e1904065800e501de
LJFan <283804672@qq.com>
2019-11-28 08:43:42 +0800

    chinese

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/chinese')
+def chinese():
+    return '这是一个中文网页'
+
+
 if __name__ == '__main__':
     print('Web Server is running...')
     app.run(port='9999')
