9908de124769f221a70c2d1fb3164b276065dc0e
LJFan <283804672@qq.com>
2019-12-04 13:56:06 +0800

    first compile renju

--- renju.cpp
+++ renju.cpp
@@ -1,7 +1,20 @@
-#include<cstdio>
+
+#include<cstring>
+#include<cassert>
 
 using namespace std;
 
-int main() {
+int solve(const char *Map) {
+    int n=strlen(Map);
+    for (int i=1;i<n;i++) {
+        if (Map[i]=='0') return i-1;
+    }
+    return -1;
+}
 
+extern "C" {
+    int renju(const char *Map) {
+        return 2;
+        return solve(Map);
+    }
 }
